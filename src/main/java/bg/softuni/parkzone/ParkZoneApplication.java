52bb86c5e4a6015e2d99850376ff19b560366b74
Dimitar Dimitrov <dimitrov.dimitar1996@gmail.com>
2026-06-17 20:01:12 +0300

    chore: setup initial project configuration

---
+++ src/main/java/bg/softuni/parkzone/ParkZoneApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.parkzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkZoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/parkzone/ParkZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.parkzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
